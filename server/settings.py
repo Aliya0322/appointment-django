cc36bc8321633d894c0037eff347eabcf91c1680
Алия Дюсембекова <DusembekovaAZ@gmail.com>
2025-03-28 20:01:36 +0300

    allowed host

--- server/settings.py
+++ server/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-#j6+9hu!zxic*xr#63(%o$qgyo&@5t9dyo)&7+pk!=dwk*gk^f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["app.mylingvoacademy.ru"]
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
